99b0121165af472e2c1428a7286898bff74b0b4e
OkiT <franta39@ya.ru>
2022-05-16 15:29:31 +0200

    task 14

--- code/lab_2/multiplicationAndDivision.php
+++ code/lab_2/multiplicationAndDivision.php
@@ -4,4 +4,4 @@ $months = 11;
 $days = 16 * $months;
 $daysPerLanguage = $days / $numLanguages;
 
-echo "It takes Meg about {$daysPerLanguage} days on average to learn a programming language.";
+echo "It takes Meg about $daysPerLanguage days on average to learn a programming language.";
